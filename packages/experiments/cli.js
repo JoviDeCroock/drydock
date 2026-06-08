0cb15297c1313c73e8707bd2d321f90499186a8a
Jovi De Croock <decroockjovi@gmail.com>
2026-06-08 18:29:06 +0200

    Remove package

--- packages/experiments/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { listExperiments, packageName } from "./index.js";
-
-const rows = listExperiments()
-  .map((experiment) => `- ${experiment.name}: ${experiment.enabled ? "on" : "off"}`)
-  .join("\n");
-
-console.log(packageName);
-console.log(rows);
